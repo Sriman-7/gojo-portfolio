3ebd2c3e45f4ac43b9ec37bc839275fe3648016b
Sriman-7 <kit29.25bsc054@gmail.com>
2026-03-13 19:16:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// click to enter
-let enter = document.getElementById("enter-screen")
+let enter = document.getElementById("enter")
 let music = document.getElementById("music")
 
 enter.onclick = () => {
@@ -7,7 +6,6 @@ enter.style.display="none"
 music.play()
 }
 
-// typing animation
 const text = "Cyber Security • Gamer • YouTuber"
 let i = 0
 
@@ -20,3 +18,39 @@ setTimeout(typing,60)
 }
 
 typing()
+
+const canvas = document.getElementById("particles")
+const ctx = canvas.getContext("2d")
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+let particles = []
+
+for(let i=0;i<100;i++){
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+size:Math.random()*3,
+speedX:Math.random()*1-0.5,
+speedY:Math.random()*1-0.5
+})
+}
+
+function animate(){
+ctx.clearRect(0,0,canvas.width,canvas.height)
+
+particles.forEach(p=>{
+p.x+=p.speedX
+p.y+=p.speedY
+
+ctx.fillStyle="white"
+ctx.beginPath()
+ctx.arc(p.x,p.y,p.size,0,Math.PI*2)
+ctx.fill()
+})
+
+requestAnimationFrame(animate)
+}
+
+animate()
